7945d8bb130782420313a22b31cfc56ab41b0a13
Hasan Farras <251837726+hfarras@users.noreply.github.com>
2026-03-06 10:41:38 +0700

    Update 2203A.cpp
    Change the code for faster time complexity

--- CodeForces-2203A/Source Code/2203A.cpp
+++ CodeForces-2203A/Source Code/2203A.cpp
@@ -1,26 +1,13 @@
 #include<bits/stdc++.h>
-using namespace std;
-
-int cont_tow(float n, float m, float d) {
-    float stack = floor((d+m)/m);
-    int tow = 0;
-    while(n >= stack) { //divide while n is greater than stack 
-        tow++;
-        n -= stack;
-    }
-    if(n < stack && n > 0) tow++; // remainder of n
-    return tow;
-}
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
     int t;
-    cin >> t;
+    scanf("%d", &t);
     while(t--) {
-        float n, m, d;
-        cin >> n >> m >> d;
-        cout << cont_tow(n, m, d) << "\n";
+        int n, m, d;
+        scanf("%d %d %d", &n, &m, &d);
+ 
+        int k = (d / m) + 1;          // maximum boxes per tower
+        int towers = (n + k - 1) / k; // ceil(n / k)
+        printf("%d\n", towers);
     }
 }
