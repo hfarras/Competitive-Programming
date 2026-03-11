df4354d894510e78c90913ee4a68d2bc1909e7bd
Hasan <farrashasan29@gmail.com>
2026-03-11 09:59:59 +0700

    Update 2203A.cpp

--- CodeForces-2203A/Source Code/2203A.cpp
+++ CodeForces-2203A/Source Code/2203A.cpp
@@ -6,8 +6,8 @@ int main() {
         int n, m, d;
         scanf("%d %d %d", &n, &m, &d);
  
-        int k = (d / m) + 1;          // maximum boxes per tower
-        int towers = (n + k - 1) / k; // ceil(n / k)
+        int k = (d / m) + 1;
+        int towers = (n + k - 1) / k;
         printf("%d\n", towers);
     }
 }
